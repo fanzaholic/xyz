4d85eab20dfb584eea9d4438a2eacbbb8b297fe5
fanzaholic <fanzaholic@yahoo.com>
2014-09-10 14:54:11 +0700

    bikin Core

--- app/controllers/welcome.php
+++ app/controllers/welcome.php
@@ -1,25 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once APPPATH . 'core/APP_Frontend.php';
+class Welcome extends APP_Frontend {
 
-class Welcome extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+		if(!$this->_check_session()){
+			redirect('auth/login');
+			exit;
+		}
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$css = "
+		body{ background-color: #f00; }
+		";
+		$this->_addStyle($css,'embed');
+
+		$js = "
+		alert('keren bro');
+		";
+		$this->_addScript($js,'embed');
+
+		$data = array('pesan' => 'Hahahahaha ini home ya!');
+		$this->_addContent('home',$data);
+		$this->_render();
 	}
 }
 
